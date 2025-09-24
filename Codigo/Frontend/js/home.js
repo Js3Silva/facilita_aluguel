8d43cdf64c39f3327542468c33581d2134d43fdc
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-09-24 01:18:16 -0300

    feat : prototipo Home, Agente e Pedidos Cliente

---
+++ Codigo/Frontend/js/home.js
@@ -0,0 +1,2 @@
+// Você pode adicionar interações extras aqui no futuro
+console.log("Página carregada!");
---
+++ Codigo/Frontend/js/pedidos.js
@@ -0,0 +1,3 @@
+document.getElementById("rowsPerPage").addEventListener("change", function () {
+  alert("Você selecionou " + this.value + " linhas por página.");
+});
